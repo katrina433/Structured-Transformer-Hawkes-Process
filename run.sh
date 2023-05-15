1d1ce0020616add2ccd2583a510e1efb256c7739
Katrina <kw433@cornell.edu>
2023-05-14 22:35:44 -0400

    Original THP

---
+++ run.sh
@@ -0,0 +1,21 @@
+device=0
+# data=data/data_so/fold1/
+# data=./../NeuralHawkesData/data_retweet/
+# data=./../NeuralHawkesData/data_mimic/fold1/
+data=./../../structuredData/
+batch=4
+n_head=4
+n_layers=4
+d_model=512
+d_rnn=64
+d_inner=1024
+d_k=512
+d_v=512
+dropout=0.1
+lr=1e-4
+smooth=0.1
+# epoch=100
+epoch=20
+log=just_testing.txt
+
+CUDA_DEVICE_ORDER=PCI_BUS_ID CUDA_VISIBLE_DEVICES=$device python Main.py -data $data -batch $batch -n_head $n_head -n_layers $n_layers -d_model $d_model -d_rnn $d_rnn -d_inner $d_inner -d_k $d_k -d_v $d_v -dropout $dropout -lr $lr -smooth $smooth -epoch $epoch -log $log
